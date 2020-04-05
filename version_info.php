8d66e0b4ecf95f8bb093dde35af318d9e589e9c7
François Kooman <fkooman@tuxed.net>
2020-04-05 22:55:16 +0200

    update version

--- version_info.php
+++ version_info.php
@@ -9,7 +9,7 @@
 
 // set this to the latest version of vpn-user-portal
 // @see https://github.com/eduvpn/vpn-user-portal/releases
-$latestVersion = '2.2.3';
+$latestVersion = '2.2.5';
 
 // discovery files
 $discoFiles = [
